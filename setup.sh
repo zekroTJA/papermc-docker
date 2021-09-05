68267107ab0eebcee59c0b7e97f77a5aa9ab497d
Ringo Hoffmann <riho@live.de>
2021-09-05 18:38:11 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
 
+function exists {
+    which $1 > /dev/null 2>&1
+}
+
+function pull {
+    exists "asd" && {
+        git clone --depth=1 $1 .
+    } || {
+        curl -Lo master.tar.gz "$1/archive/refs/heads/master.tar.gz"
+        tar -xzf master.tar.gz
+        mv papermc-docker-master/* .
+        rm -rf papermc-docker-master
+        rm -rf master.tar.gz
+    }
+}
+
+VOLDIR="papermc"
+
+TDIR=$1
+[ -z ${TDIR} ] && TDIR="./papermc"
+
 set -x
 set -e
 
-mkdir paper
-cd paper
-git clone https://github.com/zekroTJA/papermc-docker .
-mkdir papermc
+mkdir ${TDIR}
+cd ${TDIR}
+pull https://github.com/zekroTJA/papermc-docker
+mkdir ${VOLDIR}
 touch \
-    papermc/spigot.yml \
-    papermc/server.properties \
-    papermc/permissions.yml \
-    papermc/paper.yml \
-    papermc/help.yml \
-    papermc/commands.yml \
-    papermc/bukkit.yml
-echo '[]' > papermc/whitelist.json
-echo '[]' > papermc/usercache.json
-echo '[]' > papermc/ops.json
-echo '[]' > papermc/banned-players.json
-echo '[]' > papermc/banned-ips.json
+    ${VOLDIR}/spigot.yml \
+    ${VOLDIR}/server.properties \
+    ${VOLDIR}/permissions.yml \
+    ${VOLDIR}/paper.yml \
+    ${VOLDIR}/help.yml \
+    ${VOLDIR}/commands.yml \
+    ${VOLDIR}/bukkit.yml
+echo '[]' | tee \
+    ${VOLDIR}/whitelist.json \
+    ${VOLDIR}/usercache.json \
+    ${VOLDIR}/ops.json \
+    ${VOLDIR}/banned-players.json \
+    ${VOLDIR}/banned-ips.json
 docker-compose up -d --build
