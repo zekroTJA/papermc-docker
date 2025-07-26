c662f93e1f24bf48475734ded59dca8b7dde262d
Skillkiller <seblsebastian@aol.de>
2025-07-27 00:15:25 +0200

    Update get latest build script to v3

--- scripts/get-latest-build.sh
+++ scripts/get-latest-build.sh
@@ -14,6 +14,16 @@ if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
     VERSION=$(sh "$(dirname "$0")"/get-latest-version.sh)
 fi
 
-RES=$(curl -sL "$API_ENDPOINT/projects/paper/versions/$VERSION")
-echo "$RES" | jq -rM '.builds[-1]'
+# This query is based on the example published by papermc.
+# https://docs.papermc.io/misc/downloads-api/#getting-the-latest-stable-build-number
+# Archived version: https://web.archive.org/web/20250726220949/https://docs.papermc.io/misc/downloads-api/#getting-the-latest-stable-build-number
 
+LATEST_BUILD=$(curl -s -H "User-Agent: $USER_AGENT" "${API_ENDPOINT}/projects/paper/versions/${VERSION}/builds" | \
+  jq -r 'map(select(.channel == "STABLE")) | .[0] | .id')
+
+if [ "$LATEST_BUILD" != "null" ]; then
+  echo "$LATEST_BUILD"
+else
+  echo "No stable build for version $MINECRAFT_VERSION found"
+  exit 1
+fi
